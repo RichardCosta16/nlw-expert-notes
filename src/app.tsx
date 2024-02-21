dd6e37d4412242f80b507a5c3182ff54f1893b9a
richardcosta16 <richardluizrcosta@gmail.com>
2024-02-21 17:37:45 -0300

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,52 @@
+import logo from './assets/logonlwexpert.svg'
+
+export function App() {
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="NLW Expert"/>
+
+      <form action="w-full">
+        <input 
+          type="text" 
+          placeholder='Busque em suas notas...' 
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slay-500" 
+        />  
+      </form>
+
+      <div className="h-px bg-slate-700"/>
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <div className="rounded-md bg-slate-700 p-5 space-y-3"> 
+          <span className="text-sm font-medium text-slate-200">
+            Adicionar nota
+          </span>
+
+          <p className="text-sm leading-6 text-slate-400">
+            Grave uma nota em áudio que será convertida para texto automaticamente
+          </p> 
+        </div> 
+
+        <div className="rounded-md bg-slate-800 p-5 space-y-3"> 
+          <span className="text-sm font-medium text-slate-300">
+            há 2 dias
+          </span>
+
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis sed praesentium quod nihil quibusdam vero reiciendis, velit minima dolorum suscipit doloribus aut. Doloremque perferendis dolorum dicta similique, vel quasi corporis?
+          </p> 
+        </div> 
+
+        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden"> 
+          <span className="text-sm font-medium text-slate-300">
+            há 4 dias
+          </span>
+
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, vitae? Ab, fugit dicta reprehenderit laborum fuga est! Asperiores, quisquam accusantium? Assumenda itaque sunt impedit accusamus aliquid earum sed veniam a.
+          </p> 
+        </div> 
+
+      </div>
+    </div>
+  )
+}
